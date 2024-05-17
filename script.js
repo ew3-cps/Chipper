4a607d9d2775f67a1b9f81aac0add9c1316a30d9
ew3-cps <147647863+ew3-cps@users.noreply.github.com>
2024-05-17 16:56:43 +0000

    added function to form

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 var burritoForm = document.getElementById("burritoForm");
+var displayBurrito = document.getElementById("burritoResult");
 burritoForm.addEventListener("submit", function(event){
   var data = new FormData(burritoForm);
   var rice = data.get("rice");
   var beans = data.get("beans");
   var protein = data.get("protein");
-  var displayBurrito = document.getElementById("burritoResult");
-  displayBurrito.innerHTML = rice + beans + protein;
-  
+  sessionStorage.setItem("burritoRice", rice);
+  sessionStorage.setItem("burritoBeans", beans);
+  sessionStorage.setItem("burritoProtein", protein);
+  event.preventDefault();
+});
+
+var dispOrderButton = document.getElementById("dispOrderButton");
+var displayRice = sessionStorage.getItem("burritoRice");
+var displayBeans = sessionStorage.getItem("burritoBeans");
+var displayProtein = sessionStorage.getItem("burritoProtein");
+
+dispOrderButton.addEventListener("click", function(){
+  displayBurrito.innerHTML = "You ordered a burrito with " + displayRice + " rice, " + displayBeans + " beans, and " + displayProtein + " as your protein.";
 });
